9feadb754a7d8f390651d79b8fee19a7841cbdd1
d.vorobyev1 <d.vorobyev1@tinkoff.ru>
2021-03-18 23:10:44 +0300

    [lesson 2] mergeSort + CountingSort

---
+++ Lecture2/CountingSort/CountingSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace CountingSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayLength = int.Parse(Console.ReadLine());
+            var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            CountingSort(array, 10000);
+            Console.WriteLine(string.Join(' ', array));
+        }
+
+        private static void CountingSort(int[] array, int maxLengthModule)
+        {
+            var counters = new int[maxLengthModule * 2 + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                counters[maxLengthModule + array[i]]++;
+            }
+
+            var index = 0;
+            for (int j = 0; j < counters.Length; j++)
+            {
+                for (int k = 0; k < counters[j]; k++)
+                {
+                    array[index] = j - maxLengthModule;
+                    index++;
+                }
+            }
+        }
+    }
+}
---
+++ Lecture2/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayLength = int.Parse(Console.ReadLine());
+            var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            MergeSort(array);
+            Console.WriteLine(string.Join(' ', array));
+        }
+
+        private static int[] MergeSort(int[] array)
+        {
+            if (array.Length < 2)
+            {
+                return array;
+            }
+            
+            // divide part
+            var leftElementsCount = array.Length / 2;
+            var rightElementsCount = array.Length - leftElementsCount;
+
+            var leftArray = MergeSort(array.Take(leftElementsCount).ToArray());
+            var rightArray = MergeSort(array.TakeLast(rightElementsCount).ToArray());
+            
+            // merge part
+
+            var leftIndex = 0;
+            var rightIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (leftIndex >= leftArray.Length)
+                {
+                    array[i] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+                else if (rightIndex >= rightArray.Length)
+                {
+                    array[i] = leftArray[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    var leftIsLess = leftArray[leftIndex] < rightArray[rightIndex];
+                    array[i] = leftIsLess ? leftArray[leftIndex] : rightArray[rightIndex];
+                    if (leftIsLess)
+                    {
+                        leftIndex++;
+                    }
+                    else
+                    {
+                        rightIndex++;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
